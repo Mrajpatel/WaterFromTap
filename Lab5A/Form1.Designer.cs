807fb29b5b399787a0b0a62e1a2e560456a985bd
Raj Pankajkumar Patel <000744572@mohawkcollege.ca>
2018-01-23 10:05:20 -0500

    Add project files.

---
+++ Lab5A/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Lab5A
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(31, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(190, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(31, 164);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 100);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 3;
+            this.trackBar1.Location = new System.Drawing.Point(31, 102);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(183, 56);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(415, 558);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Drop in The Bucket";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Lab5A/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ *  Name: Raj Patel - 000744572
+ *  Date: 2018-01-12
+ *  
+ *  The main perpose of this class is to create an windows form application
+ *  This class is a good representation how to use windows drawing library
+ *  The graphics shown in this class are just lines and rectangle
+ *  It uses rectangle to create liquid falling effect from the tap and lines to draw and fill the bucket
+ *  
+ */
+
+/// <Statement_of_authorship>
+///     I, Raj Patel, 000744572 certify that this material is my original work.
+///     No other person's work has been used without due acknowledgement.
+/// </Statement_of_authorship>
+namespace Lab5A
+{
+
+    public partial class Form : System.Windows.Forms.Form
+    {
+        
+        private Pen pen;                                //Pen to draw line over each other -> filling effect
+        private SolidBrush brush;                       //brush to fill up the rectangle from the tap
+        SolidBrush erase = new SolidBrush(Color.Black); //brush to remove the vertical rectangle
+        private Graphics objectA;                       //creating a new graphics object
+        private Color c = Color.LightBlue;              //setting the default color to light-blue
+        private int x = 440;                            //coordinate to locate the top line of filling bucket
+        private int y = 0;                              //cordinate to locate the points of the new line each time
+        private bool flag = false;                      //creating a boolean variable to set the defalult values
+        public Form()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(frmMain_Paint);         //Registers the Paint event handler
+
+        }
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            objectA = e.Graphics;
+            pen = new Pen(Color.White);                     //setting the color of line to draw bucket to white
+            objectA = this.CreateGraphics();                //Create a graphics object
+            objectA.DrawLine(pen, 75, 300, 75, 440);        //drawing the left line of bucket
+            objectA.DrawLine(pen, 280, 300, 280, 440);      //drawing the right line of bucket
+            objectA.DrawLine(pen, 75, 440, 280, 440);       //drawing the base of bucket
+            //objectA.DrawLine(pen, 75, 430, 280, 430);
+            //objectA.FillRectangle(brush, 75, 320, 206, 120); //drawing a rectangle
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //opening the color selection box
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                c = colorDialog1.Color;                     //if the color is selected set it to variable
+            }
+            else
+            {
+                c = Color.LightCoral;                       //set the default color
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //create a condition that if the flag is hit re-paint the color
+            if (flag)
+            {
+                erase = new SolidBrush(Color.Black);                //if the flag is hit set the color to black
+                objectA.FillRectangle(erase, 76, 320, 204, 260);    //paint the bucket with black color
+                flag = false;                                       //set the flag to false so it don't hit again
+            }
+            //set the color selected to brush
+            brush = new SolidBrush(c);
+            //set the graphics
+            objectA = this.CreateGraphics();
+            //if the trackBar is set to 0 erase the vertical rectangle
+            if (trackBar1.Value == 0)
+            {
+                objectA.FillRectangle(erase, 90, 180, 20, 260 - y);
+                //stop the timer
+                timer.Stop();
+            }
+            //if the tracker is running
+            else
+            {
+                //set the interval to 100 miliseconds
+                timer.Interval = (100);
+                //set the variable to increase the speed of the water filling effect
+                int a = (trackBar1.Value * 9);
+                //decrease the timer interval to increase the speed of the drawing
+                timer.Interval = timer.Interval - a;
+                //start the timer
+                timer.Start();
+            }
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //decrease the x coordinate of the vertical rectangle
+            x -= 1;
+            //increase the height of new line each time timer function is called
+            y += 1;
+            //set the new pen to draw the new color line
+            pen = new Pen(c);
+            //if the height of line has not reached the top of bucket 
+            if (y <= 120)
+            {
+                //set the new color selected and draw the vertical rectangle
+                brush = new SolidBrush(c);
+                objectA.FillRectangle(brush, 90, 180, 20, 260 - y);         //draw the rectangle
+                objectA.DrawLine(pen, 76, x, 279, x);                       //draw the new line each time with different height
+            }
+            else
+            {
+                timer.Stop();                                           //stop the timer if the height is reached
+                trackBar1.Value = 0;                                    //set the tracker back to 0
+                flag = true;                                            //set the flag to true so it hits when called again
+                objectA.FillRectangle(erase, 90, 180, 20, 140);         //erase the vertical rectangle from the tap to the last line in bucket
+                y = 0;                                                  //re-setting the default coordinates 
+                x = 440;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);                            //close the console when button is hit
+        }
+    }
+}
